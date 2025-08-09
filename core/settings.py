bafad755e8768c64f783dcc8e996512d157a65e9
Soubhagya-biswal <biswalsoubhagya228@gmail.com>
2025-08-09 16:21:14 +0530

    Final Fix: Admin panel CSS theek kiya

--- core/settings.py
+++ core/settings.py
@@ -93,8 +93,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
-# Extra setting for WhiteNoise (Fast delivery boy)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
