285d8d8e00b9639984d402bd8ce33944040286a4
Soubhagya-biswal <biswalsoubhagya228@gmail.com>
2025-08-11 06:24:47 +0530

    TABAHAHI-CLEANUP: Updated .gitignore and cleaned Git cache

---
+++ store/audit_logging.py
@@ -0,0 +1,11 @@
+from auditlog.registry import auditlog
+from django.contrib.auth.models import User
+from .models import Product, Order, Address, Review
+
+auditlog.register(User)
+auditlog.register(Product)
+auditlog.register(Order)
+auditlog.register(Address)
+auditlog.register(Review)
+
+print("TABAHAHI LOGGING: Saare cameras (models) register ho gaye hain!")
